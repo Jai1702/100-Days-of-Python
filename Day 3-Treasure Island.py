a87079b200c28c9e22f9649eb5374afd1f246977
Jai1702 <jai300463@gmail.com>
2025-07-13 12:13:41 -0400

    Day 3-Treasure Island.py

--- Day 3-Treasure Island.py
+++ Day 3-Treasure Island.py
@@ -27,12 +27,12 @@ direction = input('You\'re at a crossroad, where do you want to go? Type R for r
 if direction == "L":
     swim_or_wait = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.\n')
     if swim_or_wait == "wait":
-        choice3 = input("You arrive at the island unharmed. There is house with 3 doors. One red, one yellow and one blue. Which colour do you choose?\n")
-        if choice3 == "red":
+        door = input("You arrive at the island unharmed. There is house with 3 doors. One red, one yellow and one blue. Which colour do you choose?\n")
+        if door == "red":
             print("It's a room full of fire. Game Over")
-        elif choice3 == "yellow":
+        elif door == "yellow":
             print("You found the treasure. You Win!")
-        elif choice3 == "blue":
+        elif door == "blue":
             print("You enter a room of beasts. Game Over.")
         else:
             print("You chose a door that doesn't exist. Game Over.")
